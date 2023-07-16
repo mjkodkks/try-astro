1da9e95e792c54543a484d337c8b073ab1849040
DKKs <eye.q.21@gmail.com>
2023-07-17 05:28:09 +0700

    [Add] deploy ghpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), vue(), svelte()]
+  integrations: [tailwind(), react(), vue(), svelte()],
+  site: 'https://github.com/mjkodkks',
+  base: '/try-astro',
 });
